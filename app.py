f30e46d4a45a67b56ba2075cc8e338819f6fe316
Cristian PC <cristianpc3087@outlook.com>
2024-09-19 11:34:54 -0500

    [FIX] ejecutando mi 2do script action

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+"""Ejemplo de Flask con docker y kubernetes"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    """ Retornando un saludo Simple"""
     return "Hello, World! This is a simple Python app running in Docker and Kubernetes."
 
 if __name__ == '__main__':
